e12fdcb841ba50200ace8677888c8ca12a0d8264
Karen Stevenson <karen@elderweb.com>
2015-12-06 09:12:46 -0600

    Add code to run after devel generate to clean up the TV show content, make season and episode number sequential and add numbers to the titles

--- demo_tv_shows/create_content.sh
+++ demo_tv_shows/create_content.sh
@@ -6,8 +6,12 @@ drush genc 5 --types=tv_show
 # Generate seasons next so season references will work.
 drush genc 20 --types=tv_season
 
-# Finally generate episodes.
+# Generate episodes.
 drush genc 50 --types=tv_episode
 drush genc 50 --types=tv_episode
 drush genc 50 --types=tv_episode
 drush genc 50 --types=tv_episode
+
+# Clean up devel generated content so season and episode numbers are in order
+# and season and episode numbers are reflected in the titles.
+drush demo-tv-shows-update-content

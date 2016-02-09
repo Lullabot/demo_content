d384a6ce7178ac49c4d4ee5d36928b574e2d0ae0
Matthew Tift <mctift@gmail.com>
2016-02-09 10:43:58 -0600

    Add more video content to the video plugin

--- demo_news/src/Plugin/EmbeddedContent/Video.php
+++ demo_news/src/Plugin/EmbeddedContent/Video.php
@@ -27,6 +27,10 @@ class Video extends EmbeddedContentBase implements EmbeddedContentInterface {
   function getList() {
     return [
       '<iframe src="https://player.vimeo.com/video/116565603?title=0&byline=0&portrait=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/116565603">What Makes Lullabot Lullabot?</a> from <a href="https://vimeo.com/lullabot">Lullabot</a> on <a href="https://vimeo.com">Vimeo</a>.</p>',
+      '<video controls height="240" width="320"><source src="https://ia802605.us.archive.org/7/items/ZeldaMajorasMask_100p_655/ZeldaMajorasMask_100p_655_HQ_part01_512kb.mp4" type="video/mp4" /></video>',
+      '<video controls height="360" width="320"><source src="https://ia601400.us.archive.org/31/items/taxi-turvy/taxi-turvy.ogv" type="video/ogg" /></video>',
+      '<video controls height="240" width="320"><source src="https://ia800309.us.archive.org/2/items/Popeye_Nearlyweds/Popeye_Nearlyweds_512kb.mp4" type="video/mp4" /></video>',
+      '<video controls height="240" width="320"><source src="https://ia800303.us.archive.org/35/items/SOLDIER/SOLDIER_64k.mp4" type="video/mp4" /></video>',
     ];
   }
 }
